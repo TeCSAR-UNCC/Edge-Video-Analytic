4bd895fd82c2847df4b50d4dfbdc4df45a79625b
Matias Mendieta <mmendiet@uncc.edu>
2019-02-18 14:06:35 -0500

    Added Comments Signed-off-by: Matias Mendieta <mmendiet@uncc.edu>

--- SocketCode/Spring2019/SDserver3.cpp
+++ SocketCode/Spring2019/SDserver3.cpp
@@ -31,15 +31,13 @@ char* clientIP[NUM_NODES];
 vector<queue <int> > sQList;
 vector<queue <int> > rQList;
 
-//queue<int> sendQ;
-//queue<int> recvQ;
-
-int sendLock = 0;
-int recvLock = 1;
 
 
-
-
+/* Function: sendData
+    Use: Send data to edge node in independant thread
+    Input:
+      index: index of connedtion for stored socket and send table
+*/
 void *sendData(void* index) {
    int statIndex = *((int*) index);
    int status = 0;
@@ -51,6 +49,7 @@ void *sendData(void* index) {
     pthread_mutex_unlock(&statusLocks[statIndex]);
     if(status > 0) {
     std::cout << "send status: " << status << std::endl;
+      /* Initilize send socket*/
       int sock;
       pthread_mutex_lock(&statusLocks[statIndex]);
       char* IPADDR = clientIP[statIndex];
@@ -68,15 +67,17 @@ void *sendData(void* index) {
 	    connect(sock,(struct sockaddr *)&servaddr,sizeof(servaddr)); 
 
       int data = 5;
-
+      /* While the connection is still valid*/
       while(status > 0) {
         pthread_mutex_lock(&sendLocks[statIndex]);
         if(!sQList[statIndex].empty()) {
+          /* Read value from send queue and send it */
           data = sQList[statIndex].front();
           sQList[statIndex].pop();
           pthread_mutex_unlock(&sendLocks[statIndex]);
           int retCode = send(sock, &data, sizeof(int), 0);
           std::cout << "retCode: " << retCode << std::endl;
+          /* Check status to make sure connection is still valid*/
           pthread_mutex_lock(&statusLocks[statIndex]);
           status = nodeStatus[statIndex];
           pthread_mutex_unlock(&statusLocks[statIndex]);
@@ -89,27 +90,34 @@ void *sendData(void* index) {
   }
 }
 
-
+/* Function: recData
+    Use: Recv data from edge node in independant thread
+    Input:
+      index: index of connedtion for stored socket and send table
+*/
 void *recvData(void* index) {
   int statIndex = *((int*) index);
   int data = 0;
           cout << "recv" << endl;
   while(1) {
+    /* Wait for status to be 1 before running */
     pthread_mutex_lock(&statusLocks[statIndex]);
     int status = nodeStatus[statIndex];
     pthread_mutex_unlock(&statusLocks[statIndex]);
 
     if(status > 0)  {
+      /* return code for recv function */
       int retCode = 1;
       while(retCode > 0) {
         retCode = recv(comm_fd[statIndex], &data, sizeof(int), 0);
-        
+        /* Lock and push data into queue */
         pthread_mutex_lock(&recvLocks[statIndex]);
         rQList[statIndex].push(data);
         pthread_mutex_unlock(&recvLocks[statIndex]);
     
         cout << data << " recv retCode: " << retCode << endl;
       }
+      /*When disconnected, close socket*/
       pthread_mutex_lock(&statusLocks[statIndex]);
       nodeStatus[statIndex] = 0;
       pthread_mutex_unlock(&statusLocks[statIndex]);
@@ -119,14 +127,23 @@ void *recvData(void* index) {
   }
 }
 
+
+/* Function: listenFunc
+    Use: Listen and accept connections in independant thread
+*/
 void *listenFunc(void*) {
+
+  /* Initilize Thread IDs for send and recv*/
   pthread_t recvThreads[NUM_NODES];
   pthread_t sendThreads[NUM_NODES];
 
+  /* Index used for thread organization*/
   int statusIndex[NUM_NODES];
 
+  /* Initilize socket for listening*/
    int listen_fd;
 
+    /* Initilize server: General structure*/
 	  struct sockaddr_in servaddr;
     struct sockaddr_in clientAddr;
     socklen_t clLen = sizeof(clientAddr);
@@ -141,6 +158,7 @@ void *listenFunc(void*) {
 
 	  bind(listen_fd, (struct sockaddr *) &servaddr, sizeof(servaddr));
 
+  /* Spawn send and recv threads*/
   for(int i = 0; i < NUM_NODES; ++i) {
     statusIndex[i] = i;
     clientIP[i] = NULL;
@@ -150,7 +168,9 @@ void *listenFunc(void*) {
   }
 
   while(1) {
+    /* Listen for new connection and accept*/
     listen(listen_fd, 10);
+    /* Store the IP address of Client in clientAddr*/
     int comm_Temp = accept(listen_fd, (struct sockaddr*) &clientAddr, &clLen);
     cout << "IPnew: " << inet_ntoa(clientAddr.sin_addr) << endl;
     int useIndex = -1;
@@ -159,7 +179,7 @@ void *listenFunc(void*) {
         useIndex = i;
       }
       else {
-        if(!strcmp(clientIP[i], inet_ntoa(clientAddr.sin_addr))) {
+        if(strcmp(clientIP[i], inet_ntoa(clientAddr.sin_addr))==0) {
           //cout << "IPnew: " << inet_ntoa(clientAddr.sin_addr) << endl;
           //cout << "IPold: " << clientIP[i] << endl;
           useIndex = i;
@@ -188,43 +208,49 @@ void *listenFunc(void*) {
 
 }
 
-//g++ SDclient3.cpp -o SDclient3
-
+/* Function: main
+    Use: Initilize listen thread and handle test data
+*/
 int main(int argc, char const *argv[]) {
 
   for(int i = 0; i < NUM_NODES; ++i) {
+    /* Intialize Vector of Queues for send and recieve data*/
     sQList.push_back(queue<int>());
     rQList.push_back(queue<int>());
-
+    /* Initializing all Mutexes */
     pthread_mutex_init(&sendLocks[i], NULL);
     pthread_mutex_init(&recvLocks[i], NULL);
     pthread_mutex_init(&statusLocks[i], NULL);
   }
 
+  /* Create thread ID for listen Thread*/
   pthread_t listenThread;
+  /* Launch thread to listen for incoming connections*/
   pthread_create(&listenThread, NULL, listenFunc, NULL);
 
   sleep(3);
   int j = 0;
   while(1) {
 
-  for(int i = 0; i < NUM_NODES; ++i) {
-    pthread_mutex_lock(&sendLocks[i]);
-    sQList[i].push(j);
-    pthread_mutex_unlock(&sendLocks[i]);
-  }
-
-  for(int i = 0; i < NUM_NODES; ++i) {
-    pthread_mutex_lock(&recvLocks[i]);
-    if(!rQList[i].empty()) {    
-      rQList[i].pop();
+    /* Push values to send into send queue*/
+    for(int i = 0; i < NUM_NODES; ++i) {
+      pthread_mutex_lock(&sendLocks[i]);
+      sQList[i].push(j);
+      pthread_mutex_unlock(&sendLocks[i]);
     }
-    pthread_mutex_unlock(&recvLocks[i]);
-  }
-    ++j;
-  if(j%30==0) {
-    sleep(3);
-  }  
+
+    /* Pop values out of recv queue*/
+    for(int i = 0; i < NUM_NODES; ++i) {
+      pthread_mutex_lock(&recvLocks[i]);
+      if(!rQList[i].empty()) {    
+        rQList[i].pop();
+      }
+      pthread_mutex_unlock(&recvLocks[i]);
+    }
+      ++j;
+    if(j%30==0) {
+      sleep(3);
+    }  
  }
 
 
